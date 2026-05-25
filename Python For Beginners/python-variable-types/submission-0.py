170d0776e79adcef0eb46f713a6756a62837879e
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-05-25 15:15:50 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 34
+float_type = 32.22
+bool_type = False
+string_type = "Jehovah"
+list_type = [7,3,7]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
