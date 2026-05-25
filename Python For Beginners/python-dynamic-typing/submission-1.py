d0fcebfe4f114befe91329a7a62b6d5731fb5dfd
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-05-25 15:19:47 -0400

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.23
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Poornima"
+print(type(variable))
+variable = [2,4,5,6]
+print(type(variable))
