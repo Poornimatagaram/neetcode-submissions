7982efbd487a0ac3f44e15c674e8fb7b2c262c58
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-06-03 23:38:00 -0400

    Add: python-string-indexing - submission-0

---
+++ Python For Beginners/python-string-indexing/submission-0.py
@@ -0,0 +1,21 @@
+def print_first_char(word: str) -> None:
+    firstchar = word[0]
+    return firstchar
+
+def print_second_char(word: str) -> None:
+    secondchar = word[1]
+    return secondchar
+
+def print_last_char(word: str) -> None:
+    lastchar = word[-1]
+    return lastchar
+
+
+# do not modify below this line
+print_first_char("hello")
+print_second_char("hello")
+print_last_char("hello")
+
+print_first_char("yay")
+print_second_char("yay")
+print_last_char("yay")
