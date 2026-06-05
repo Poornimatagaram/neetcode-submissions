1ce47e2a103dc091ff93c25d8f827c3f10ccf9b3
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-06-04 23:23:24 -0400

    Add: python-string-looping - submission-1

---
+++ Python For Beginners/python-string-looping/submission-1.py
@@ -0,0 +1,10 @@
+def print_string_characters(my_string: str) -> None:
+    string_len= len(my_string)
+
+    for i in range(string_len):
+        print(my_string)
+
+
+# do not modify below this line
+print_string_characters("Hello, World!")
+print_string_characters("Good Job!")
