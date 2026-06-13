d7b4ca3bf0bd5bb35f3170e3276d7926891a36d0
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-06-13 12:34:33 -0400

    Add: python-list-operations - submission-1

---
+++ Python For Beginners/python-list-operations/submission-1.py
@@ -0,0 +1,23 @@
+def check_list_empty(my_list) -> bool:
+    if my_list:
+        print("The list is not empty")
+    else:
+        print("The list is empty")    
+
+
+def check_element_in_list(my_list, element) -> bool:
+    if element in my_list:
+        print(f" {element} is present in the list")
+    else:
+        print(f" {element} is not present in the list")    
+
+
+# do not modify below this line
+print(check_list_empty([]))
+print(check_list_empty([1, 2, 3]))
+
+print(check_element_in_list([1, 2, 3], 1))
+print(check_element_in_list([1, 2, 3], 4))
+
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Banana"))
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Grape"))
