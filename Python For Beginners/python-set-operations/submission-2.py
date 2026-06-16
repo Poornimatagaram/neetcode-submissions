3b300e4f77f17c97f17f558a1f195222f413a9aa
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-06-15 20:26:57 -0400

    Add: python-set-operations - submission-2

---
+++ Python For Beginners/python-set-operations/submission-2.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def count_unique_words(words: List[str]) -> int:
+
+    if words is None:
+        return 0
+
+    new_set = set(words) 
+    return  len(new_set)
+    
+        
+   
+
+       
+
+# do not modify code below this line
+print(count_unique_words(["hello", "world", "hello", "goodbye"]))
+print(count_unique_words(["hello", "world", "i", "am", "world"]))
+print(count_unique_words(["hello", "hello", "hello"]))
+print(count_unique_words([]))
