b993b5259557b4aaada35fdbdd7d8f40b17569bf
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-06-28 18:59:36 -0400

    Add: python-dict-operations - submission-0

---
+++ Python For Beginners/python-dict-operations/submission-0.py
@@ -0,0 +1,15 @@
+your_dict = { 
+  "a": 10, 
+  "apple": 12,
+  "bat": 7
+}
+for d in your_dict:
+  print(True)
+else:
+  print(False)  
+
+your_dict["a"] = 4  
+
+print(your_dict)
+print(your_dict["a"])
+print(your_dict)
