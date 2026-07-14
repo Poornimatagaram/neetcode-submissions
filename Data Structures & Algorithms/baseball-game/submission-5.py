691a364bdd13ed0c54a5252bece482bbe70bdf15
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-07-13 23:56:20 -0400

    Add: baseball-game - submission-5

---
+++ Data Structures & Algorithms/baseball-game/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        record_output = []
+        for i in range(len(operations)):
+            if i == int:
+                record_output.append(x)
+            elif i == "+":
+                record_output.sum()
+
+            elif i == "C":
+                record_output.pop()
+
+            elif i =="D":
+                record_output.append(range(len()))
+
+            else:
+                record_output    
+
+        return record_output
+
+finPoints = Solution()
+print(finPoints.calPoints)
+                
+        
