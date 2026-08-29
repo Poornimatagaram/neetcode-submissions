15d4b79f7bce998ebef85461b7c458efdc82302f
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-08-28 21:01:45 -0400

    Add: python-loop-unpacking - submission-10

---
+++ Python Coding Interviews/python-loop-unpacking/submission-10.py
@@ -0,0 +1,20 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    num=scores[0][1]
+    print(num)
+    for i,j in scores:
+        
+        print(f"i: {i}")
+        print(f"j:{j}")
+    
+
+
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
