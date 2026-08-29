3073c56401b74d6a4719aa96f16453ea155a77b0
Poornima Tagaram <89180592+Poornimatagaram@users.noreply.github.com>
2026-08-28 21:05:20 -0400

    Add: python-loop-unpacking - submission-13

---
+++ Python Coding Interviews/python-loop-unpacking/submission-13.py
@@ -0,0 +1,18 @@
+from typing import List, Tuple
+
+
+def best_student(scores: List[Tuple[str, int]]) -> str:
+    num=scores[0][1]
+    
+    for i,j in scores:
+        if j >= num:
+            num += j
+            return i    
+
+
+
+# do not modify below this line
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 80), ("Charlie", 100)]))
+print(best_student([("Alice", 90), ("Bob", 100), ("Charlie", 70)]))
+print(best_student([("Alice", 90), ("Bob", 90), ("Charlie", 80), ("David", 100)]))
